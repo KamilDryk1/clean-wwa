2ed09b02db740785eed0c8fb083b669df823af11
Kamil Dryk <kamildk8@gmail.com>
2022-04-15 22:04:25 +0200

    add header text

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,11 @@
             ></video>
             <div class="overlay"></div>
           </div>
+		  <div class="header__content-text">
+			  <h1 class="title">CLEAN-WWA</h1>
+			  <h2 class="subtitle">Pranie tapicerki meblowej i samochodowej na terenie Warszawy i okolic.</h2>
+			  <p>Poznaj naszą ofertę!</p>
+		  </div>
         </div>
       </div>
     </section>
@@ -42,6 +47,7 @@ export default {
   &__content {
     height: 700px;
     width: 100%;
+	margin: 0 8%;
 
     &-video {
       position: relative;
@@ -66,10 +72,39 @@ export default {
         left: 0;
         width: 100%;
         height: 700px;
-        background-color: rgba(0, 0, 0, 0.5);
+        background: linear-gradient(
+				to right,
+				black 0% 3%,
+				rgba(0, 0, 0, 0.4),
+				black 97% 100%
+			);
         z-index: 3;
       }
     }
+
+	&-text {
+		position: absolute;
+		top: 25%;
+		z-index: 4;
+		color: white;
+		max-width: 65%;
+
+		h1 {
+			font-size: 64px;
+		}
+
+		h2 {
+			font-weight: 400;
+			font-size: 30px;
+			margin-top: 30px;
+		}
+
+		p{
+			font-weight: 200;
+			margin-top: 30px;
+			font-size: 20px;
+		}
+	}
   }
 }
 </style>
