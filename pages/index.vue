8eff1de74e4a2758f131ea5bd11af3e01468c295
Kamil Dryk <kamil.dryk@streamonline.pl>
2022-04-19 21:45:31 +0200

    header to fix

--- pages/index.vue
+++ pages/index.vue
@@ -13,22 +13,30 @@
             ></video>
             <div class="overlay"></div>
           </div>
-		  <div class="header__content-text">
-			  <h1 class="title">CLEAN-WWA</h1>
-			  <h2 class="subtitle">Pranie tapicerki meblowej i samochodowej na terenie Warszawy i okolic.</h2>
-			  <p>Poznaj naszą ofertę!</p>
-		  </div>
+          <div class="header__content-text">
+            <h1 class="title">CLEAN-WWA</h1>
+            <h2 class="subtitle">
+              Pranie tapicerki meblowej i samochodowej na terenie Warszawy i
+              okolic.
+            </h2>
+            <p>Poznaj naszą ofertę!</p>
+          </div>
         </div>
       </div>
     </section>
-	<section class="description">
-		<div class="description__container">
-			<div class="description__content">
-				<div class="description__content-text"></div>
-				<div class="description__content-photo"></div>
-			</div>
-		</div>
-	</section>
+    <section class="description">
+      <div class="description__container">
+        <div class="description__content">
+          <div class="description__content-text"></div>
+          <div class="description__content-photo">
+            <img
+              src="assets/img/HomePage/description-img.jpg"
+              alt="Pranie tapicerki bagażnika"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -54,8 +62,7 @@ export default {
 
   &__content {
     height: 700px;
-    width: 100%;
-	margin: 0 8%;
+    margin: 0 8%;
 
     &-video {
       position: relative;
@@ -81,42 +88,66 @@ export default {
         width: 100%;
         height: 700px;
         background: linear-gradient(
-				to right,
-				black 0% 3%,
-				rgba(0, 0, 0, 0.4),
-				black 97% 100%
-			);
+          to right,
+          black 0% 3%,
+          rgba(0, 0, 0, 0.4),
+          black 97% 100%
+        );
         z-index: 3;
       }
     }
 
-	&-text {
-		position: absolute;
-		top: 25%;
-		z-index: 4;
-		color: white;
-		max-width: 65%;
+    &-text {
+      position: absolute;
+      top: 25%;
+      z-index: 4;
+      color: white;
+      max-width: 65%;
 
-		h1 {
-			font-size: 64px;
-		}
+      h1 {
+        font-size: 64px;
+      }
 
-		h2 {
-			font-weight: 400;
-			font-size: 30px;
-			margin-top: 30px;
-		}
+      h2 {
+        font-weight: 400;
+        font-size: 30px;
+        margin-top: 30px;
+      }
 
-		p{
-			font-weight: 200;
-			margin-top: 30px;
-			font-size: 20px;
-		}
-	}
+      p {
+        font-weight: 200;
+        margin-top: 30px;
+        font-size: 20px;
+      }
+    }
   }
 }
 
 .description {
-	height: 500px;
+  height: 500px;
+
+  &__container {
+    max-width: 1440px;
+    margin: 0 auto;
+  }
+
+  &__content {
+    margin: 0 8%;
+    display: flex;
+    justify-content: space-around;
+    padding-top: 150px;
+
+    &-text {
+      width: 40%;
+    }
+
+    &-photo {
+      width: 40%;
+
+      img {
+        width: 100%;
+      }
+    }
+  }
 }
 </style>
