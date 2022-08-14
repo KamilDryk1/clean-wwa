289f820dca210a19c820f353c0a8cdf4c71eff63
Kamil Dryk <kamildk8@gmail.com>
2022-08-14 17:26:22 +0200

    price list update

--- pages/cennik.vue
+++ pages/cennik.vue
@@ -1,243 +1,243 @@
 <template>
   <section class="price">
-    <div class="price__container">
-      <div class="price__content">
-        <h1 class="price__content-title">Pranie tapicerki meblowej</h1>
-        <ul class="price__content-list">
-          <li class="price__content-element">
-            <div class="key"><p>Wersalka</p></div>
-            <div class="value"><p>od 110 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Sofa 2 osobowa</p></div>
-            <div class="value"><p>od 120 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Sofa 3 osobowa</p></div>
-            <div class="value"><p>od 130 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Narożnik 3 osobowy</p></div>
-            <div class="value"><p>od 150 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Narożnik większy niż 3 os (powyżej 2.5m)</p>
-            </div>
-            <div class="value"><p>od 180 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Narożnik w kształcie litery U</p>
-            </div>
-            <div class="value"><p>od 250 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Fotel</p>
-            </div>
-            <div class="value"><p>50-80 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Krzesło</p>
-            </div>
-            <div class="value"><p>15-30 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Krzesło biurowe</p>
-            </div>
-            <div class="value"><p>od 25 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Pufa</p>
-            </div>
-            <div class="value"><p>od 20 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Rama łóżka</p>
-            </div>
-            <div class="value"><p>od 100 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Poduszki</p>
-            </div>
-            <div class="value"><p>5-15 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Pranie materiałów plamoodpornych (np. penta, carabu)</p>
-            </div>
-            <div class="value"><p>wycena indywidualna</p></div>
-          </li>
-        </ul>
-        <h1 class="price__content-title">Usługi dodatkowe</h1>
-        <ul class="price__content-list">
-          <li class="price__content-element">
-            <div class="key"><p>Neutralizacja zapachów</p></div>
-            <div class="value"><p>30 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Impregnacja tapicerki</p></div>
-            <div class="value"><p>80% ceny prania mebla</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Podsuszanie tapicerki</p></div>
-            <div class="value"><p>20 zł</p></div>
-          </li>
-        </ul>
-        <h1 class="price__content-title">Pranie dywanu w mieszkaniu klienta</h1>
-        <ul class="price__content-list">
-          <li class="price__content-element">
-            <div class="key"><p>Dywan syntetyczny krótki włos</p></div>
-            <div class="value"><p>15 zł m2</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Dywan syntetyczny długi włos (Shaggy)</p></div>
-            <div class="value"><p>17 zł m2</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Wykładzina dywanowa</p></div>
-            <div class="value"><p>15 zł m2</p></div>
-          </li>
-        </ul>
-        <h1 class="price__content-title">Pranie wykładziny biurowej</h1>
-        <ul class="price__content-list">
-          <li class="price__content-element">
-            <div class="key"><p>Wykładzina biurowa do 100 m2</p></div>
-            <div class="value"><p>12 zl m2</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Wykładzina biurowa od 100 m2</p></div>
-            <div class="value"><p>wycena indywidualna</p></div>
-          </li>
-        </ul>
-        <h1 class="price__content-title">Pranie materacy</h1>
-        <ul class="price__content-list">
-          <li class="price__content-element">
-            <div class="key">
-              <p>
-                Pranie materaca jednoosobowego z jednej strony / z obu stron
-              </p>
-            </div>
-            <div class="value"><p>od 60/90 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Pranie materaca dwuosobowego z jednej strony / z obu stron</p>
-            </div>
-            <div class="value"><p>od 100/150 zł</p></div>
-          </li>
-        </ul>
-        <h1 class="price__content-title">Pranie tapicerki samochodowej</h1>
-        <ul class="price__content-list">
-          <li class="price__content-element">
-            <div class="key"><p>Przednie fotele plus kanap</p></div>
-            <div class="value"><p>od 160 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Czyszczenie podsufitki</p></div>
-            <div class="value"><p>100 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Odkurzanie i pranie podłogi</p></div>
-            <div class="value"><p>od 50 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Pranie boczków drzwi</p></div>
-            <div class="value"><p>od 50 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Pranie bagażnika</p></div>
-            <div class="value"><p>od 50 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Pranie dywaników</p></div>
-            <div class="value"><p>od 10 zł szt</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Czyszczenie plastików, kokpitu</p></div>
-            <div class="value"><p>od 50 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Czyszczenie foteli skórzanych</p></div>
-            <div class="value"><p>od 250 zł</p></div>
-          </li>
-          <li class="price__content-element">
-            <div class="key">
-              <p>Kompleksowe czyszczenie całego wnętrza samochodu</p>
-            </div>
-            <div class="value"><p>od 400 zł</p></div>
-          </li>
-        </ul>
-        <h1 class="price__content-title">
-          Mycie powierzchni twardych płytki, gres, posadzki, podłogi przemysłowe
-        </h1>
-        <ul class="price__content-list">
-          <li class="price__content-element">
-            <div class="key"><p>Mycie posadzki</p></div>
-            <div class="value">
-              <p>6 - 10 zł m2</p>
-            </div>
-          </li>
-        </ul>
-        <h1 class="price__content-title">
-          Wypożyczenie odkurzacza piorącego profi 50.1c, lub karcher puzzi 10/1
-        </h1>
-        <ul class="price__content-list">
-          <li class="price__content-element">
-            <div class="key"><p>Doba</p></div>
-            <div class="value">
-              <p>60 zł*</p>
-            </div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Dodatkowe 100g proszku</p></div>
-            <div class="value">
-              <p>10 zł</p>
-            </div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Odbiór osobisty</p></div>
-            <div class="value">
-              <p>0 zł</p>
-            </div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Dowóz i odbiór na terenie Warszawy</p></div>
-            <div class="value">
-              <p>40 zł (20 zł dowóz + 20 zł odbiór)</p>
-            </div>
-          </li>
-          <li class="price__content-element">
-            <div class="key"><p>Kaucja</p></div>
-            <div class="value">
-              <p>200 zł</p>
-            </div>
-          </li>
-        </ul>
-        <p class="bonus">
-          *miarka proszku (100g) Prochem Crystal Green GRATIS!
-        </p>
-        <p class="info">
-          UWAGA! - Pomimo zastosowania odpowiednich odplamiaczy nie jesteśmy w
-          stanie zagwarantować, że każda plama zostanie usunięta. Czasami zdarza
-          się, że materiał jest permanentnie odbarwiony.
-        </p>
-        <p class="info">
-          Na zamówioną usługę jest przyznawana 3-dniowa gwarancja. Jeżeli
-          państwo nie będą zadowoleni z efektu, lub po wyschnięciu będą widoczne
-          plamy przyjedziemy i poprawimy je kompletnie bezpłatnie.
-        </p>
-        <p class="info-last">
-          Minimalny koszt zamówienia usługi na terenie Warszawy - 110 zł
-        </p>
-      </div>
-    </div>
+	<div class="price__container">
+	  <div class="price__content">
+		<h1 class="price__content-title">Pranie tapicerki meblowej</h1>
+		<ul class="price__content-list">
+		  <li class="price__content-element">
+			<div class="key"><p>Wersalka</p></div>
+			<div class="value"><p>od 110 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Sofa 2 osobowa</p></div>
+			<div class="value"><p>od 120 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Sofa 3 osobowa</p></div>
+			<div class="value"><p>od 130 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Narożnik 3 osobowy</p></div>
+			<div class="value"><p>od 150 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Narożnik większy niż 3 os (powyżej 2.5m)</p>
+			</div>
+			<div class="value"><p>od 180 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Narożnik w kształcie litery U</p>
+			</div>
+			<div class="value"><p>od 250 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Fotel</p>
+			</div>
+			<div class="value"><p>50-80 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Krzesło</p>
+			</div>
+			<div class="value"><p>15-30 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Krzesło biurowe</p>
+			</div>
+			<div class="value"><p>od 25 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Pufa</p>
+			</div>
+			<div class="value"><p>od 20 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Rama łóżka</p>
+			</div>
+			<div class="value"><p>od 100 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Poduszki</p>
+			</div>
+			<div class="value"><p>5-15 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Pranie materiałów plamoodpornych (np. penta, carabu)</p>
+			</div>
+			<div class="value"><p>wycena indywidualna</p></div>
+		  </li>
+		</ul>
+		<h1 class="price__content-title">Usługi dodatkowe</h1>
+		<ul class="price__content-list">
+		  <li class="price__content-element">
+			<div class="key"><p>Neutralizacja zapachów</p></div>
+			<div class="value"><p>30 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Impregnacja tapicerki</p></div>
+			<div class="value"><p>80% ceny prania mebla</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Podsuszanie tapicerki</p></div>
+			<div class="value"><p>20 zł</p></div>
+		  </li>
+		</ul>
+		<h1 class="price__content-title">Pranie dywanu w mieszkaniu klienta</h1>
+		<ul class="price__content-list">
+		  <li class="price__content-element">
+			<div class="key"><p>Dywan syntetyczny krótki włos</p></div>
+			<div class="value"><p>15 zł m2</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Dywan syntetyczny długi włos (Shaggy)</p></div>
+			<div class="value"><p>17 zł m2</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Wykładzina dywanowa</p></div>
+			<div class="value"><p>15 zł m2</p></div>
+		  </li>
+		</ul>
+		<h1 class="price__content-title">Pranie wykładziny biurowej</h1>
+		<ul class="price__content-list">
+		  <li class="price__content-element">
+			<div class="key"><p>Wykładzina biurowa do 100 m2</p></div>
+			<div class="value"><p>12 zl m2</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Wykładzina biurowa od 100 m2</p></div>
+			<div class="value"><p>wycena indywidualna</p></div>
+		  </li>
+		</ul>
+		<h1 class="price__content-title">Pranie materacy</h1>
+		<ul class="price__content-list">
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>
+				Pranie materaca jednoosobowego z jednej strony / z obu stron
+			  </p>
+			</div>
+			<div class="value"><p>od 60/90 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Pranie materaca dwuosobowego z jednej strony / z obu stron</p>
+			</div>
+			<div class="value"><p>od 100/150 zł</p></div>
+		  </li>
+		</ul>
+		<h1 class="price__content-title">Pranie tapicerki samochodowej</h1>
+		<ul class="price__content-list">
+		  <li class="price__content-element">
+			<div class="key"><p>Przednie fotele plus kanap</p></div>
+			<div class="value"><p>od 160 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Czyszczenie podsufitki</p></div>
+			<div class="value"><p>100 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Odkurzanie i pranie podłogi</p></div>
+			<div class="value"><p>od 50 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Pranie boczków drzwi</p></div>
+			<div class="value"><p>od 50 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Pranie bagażnika</p></div>
+			<div class="value"><p>od 50 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Pranie dywaników</p></div>
+			<div class="value"><p>od 10 zł szt</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Czyszczenie plastików, kokpitu</p></div>
+			<div class="value"><p>od 50 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Czyszczenie foteli skórzanych</p></div>
+			<div class="value"><p>od 250 zł</p></div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key">
+			  <p>Kompleksowe czyszczenie całego wnętrza samochodu</p>
+			</div>
+			<div class="value"><p>od 400 zł</p></div>
+		  </li>
+		</ul>
+		<h1 class="price__content-title">
+		  Mycie powierzchni twardych płytki, gres, posadzki, podłogi przemysłowe
+		</h1>
+		<ul class="price__content-list">
+		  <li class="price__content-element">
+			<div class="key"><p>Mycie posadzki</p></div>
+			<div class="value">
+			  <p>6 - 10 zł m2</p>
+			</div>
+		  </li>
+		</ul>
+		<h1 class="price__content-title">
+		  Wypożyczenie odkurzacza piorącego profi 50.1c, lub karcher puzzi 10/1
+		</h1>
+		<ul class="price__content-list">
+		  <li class="price__content-element">
+			<div class="key"><p>Doba</p></div>
+			<div class="value">
+			  <p>60 zł*</p>
+			</div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Dodatkowe 100g proszku</p></div>
+			<div class="value">
+			  <p>10 zł</p>
+			</div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Odbiór osobisty</p></div>
+			<div class="value">
+			  <p>0 zł</p>
+			</div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Dowóz i odbiór na terenie Warszawy</p></div>
+			<div class="value">
+			  <p>40 zł (20 zł dowóz + 20 zł odbiór)</p>
+			</div>
+		  </li>
+		  <li class="price__content-element">
+			<div class="key"><p>Kaucja</p></div>
+			<div class="value">
+			  <p>200 zł</p>
+			</div>
+		  </li>
+		</ul>
+		<p class="bonus">
+		  *miarka proszku (100g) Prochem Crystal Green GRATIS!
+		</p>
+		<p class="info">
+		  UWAGA! - Pomimo zastosowania odpowiednich odplamiaczy nie jesteśmy w
+		  stanie zagwarantować, że każda plama zostanie usunięta. Czasami zdarza
+		  się, że materiał jest permanentnie odbarwiony.
+		</p>
+		<p class="info">
+		  Na zamówioną usługę jest przyznawana 3-dniowa gwarancja. Jeżeli
+		  państwo nie będą zadowoleni z efektu, lub po wyschnięciu będą widoczne
+		  plamy przyjedziemy i poprawimy je kompletnie bezpłatnie.
+		</p>
+		<p class="info-last">
+		  Minimalny koszt zamówienia usługi na terenie Warszawy - 110 zł
+		</p>
+	  </div>
+	</div>
   </section>
 </template>
 
@@ -254,66 +254,77 @@ export default {
 	background-color: #fff;
 
   &__container {
-    max-width: 1440px;
-    margin: 0 auto;
+	max-width: 1440px;
+	margin: 0 auto;
   }
 
   &__content {
-    margin: 0 4%;
-    padding: 0 4%;
-    position: relative;
-    background-color: #fff;
-    // transform: translateY(-100px);
-    z-index: 5;
+	margin: 0 4%;
+	padding: 0 4%;
+	position: relative;
+	background-color: #fff;
+	// transform: translateY(-100px);
+	z-index: 5;
 
-    &-title {
-    //   margin: 40px auto;
+	&-title {
+	//   margin: 40px auto;
 	padding: 40px 0;
-      text-align: center;
-      font-size: 30px;
-    }
+	text-align: center;
+	font-size: 30px;
+	color: #03989e;
+	}
 
-    &-list {
-      max-width: 700px;
-      margin: 0 auto;
-    }
+	&-list {
+	  max-width: 700px;
+	  margin: 0 auto;
+	}
 
-    &-element {
-      list-style: none;
-      display: flex;
-      justify-content: space-evenly;
-      border-bottom: 2px solid black;
+	&-element {
+	  list-style: none;
+	  display: flex;
+	  justify-content: space-evenly;
+	  border-bottom: 2px solid black;
 
-      div {
-        width: 50%;
-        text-align: center;
-        font-size: 18px;
-        margin: auto 0;
-      }
+	  div {
+		width: 50%;
+		text-align: center;
+		font-size: 18px;
+		margin: auto 0;
+	  }
 
-      .key {
-        border-right: 2px solid black;
-      }
-    }
+	  .key {
+		border-right: 2px solid black;
+	  }
+	}
 
-    .bonus {
-      text-align: center;
-      font-size: 12px;
-      margin-top: 2px;
-      margin-bottom: 40px;
-    }
+	&-element:nth-child(2n) {
+		background-color: #eee;
+	}
 
-    .info {
-      font-size: 14px;
-      text-align: center;
-      margin-bottom: 3px;
-    }
+	@media(min-width: 1000px) {
+		&-element:hover {
+			background-color: #bbb;
+		}
+	}
 
-    .info-last {
-      font-size: 14px;
-      text-align: center;
-      padding-bottom: 40px;
-    }
+	.bonus {
+	  text-align: center;
+	  font-size: 12px;
+	  margin-top: 2px;
+	  margin-bottom: 40px;
+	}
+
+	.info {
+	  font-size: 14px;
+	  text-align: center;
+	  margin-bottom: 3px;
+	}
+
+	.info-last {
+	  font-size: 14px;
+	  text-align: center;
+	  padding-bottom: 40px;
+	}
   }
 }
 </style>
